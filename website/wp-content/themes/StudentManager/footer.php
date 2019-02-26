0f75cc4de31b4191a67decf23c7a664c85888f4b
tommaddoxsoftware <tommaddox1997uk@gmail.com>
2019-02-26 09:47:06 +0000

    Added header and footer structure

---
+++ website/wp-content/themes/StudentManager/footer.php
@@ -0,0 +1,57 @@
+</div> <!-- End body wrapper -->
+<footer>
+    <div class="container-fluid" id="footer-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                </div>
+                <div class="col-6 text-right" id="contact-info">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid" id="footer-contrast">
+        <div class="row">
+            <div class="col-md-8" id="links">
+                <p>
+                    <strong>Student Manager 2019&copy;</strong>
+                    <a href="<?echo home_url()?>/terms-and-conditions/">Terms and Conditions</a>
+                    /
+                    <a href="<?echo home_url()?>/privacy-policy">Privacy Policy</a>
+                </p>
+            </div>
+            <div class="col-md-4" id="social">
+                <div class="row">
+                    <div class="col-3">
+                        <a href="#">
+                            <img src="<?echo bloginfo('template_directory')?>/assets/icons/social/fb-social.png" alt="social-media icon facebook"/>
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <img src="<?echo bloginfo('template_directory')?>/assets/icons/social/twitter-social.png" alt="social-media icon twitter"/>
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <img src="<?echo bloginfo('template_directory')?>/assets/icons/social/yt-social.png" alt="social-media icon youtube"/>
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="contact-us">
+                            <img src="<?bloginfo('template_directory')?>/assets/icons/social/contact-social.png" alt="social-media icon contact"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
