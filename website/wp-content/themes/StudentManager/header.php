6a671e2a143bb7e9ebb8077f498a96217c1183b0
tommaddoxsoftware <tommaddox1997uk@gmail.com>
2019-03-04 07:58:26 +0000

    Added Navbar and content wrapper to header.php

--- website/wp-content/themes/StudentManager/header.php
+++ website/wp-content/themes/StudentManager/header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?echo the_title()?> - Student Manager</title>
+    <title><?echo the_title()?> - Student manager</title>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -15,5 +15,24 @@
     <?php wp_head(); ?>
 </head>
 
-<body>
-    <div class="container-fluid body-wrapper">
+<body data-spy="scroll" data-target=".navbar" data-offset="65">
+	<nav class="navbar navbar-expand-lg navbar-dark navbar-custom bg-light ">
+		<div class="container-fluid" id="nav-row">
+			<img class="img-responsive logo" src="<?bloginfo('template_directory')?>/assets/logos/logo.png" height="120"/>
+			<button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#sm-navbar-collapse" aria-controls="sm-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+				<?
+				wp_nav_menu( array(
+				    'theme_location'  => 'primary',
+				    'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+				    'container'       => 'div',
+				    'container_class' => 'collapse navbar-collapse',
+				    'container_id'    => 'sm-navbar-collapse',
+				    'menu_class'      => 'navbar-nav mx-auto',
+				    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+				    'walker'          => new WP_Bootstrap_Navwalker(),
+				) );
+				?>
+	    </nav>
+        <div class="container-fluid"> <!-- Begin Body Content -->
